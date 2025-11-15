2cdb698fc1d5dbb51bb27243673f9c6c9f8e8916
rafi <rafi2002ten@gmail.com>
2025-11-15 14:28:32 +0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tawhid on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
